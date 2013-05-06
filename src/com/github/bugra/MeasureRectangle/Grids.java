c0f55703c4148a62bc010897748fbda43acc7ad4
bugra <bugraa@live.com>
2013-05-06 10:51:16 -0400

    Added different modes

--- src/com/github/bugra/MeasureRectangle/Grids.java
+++ src/com/github/bugra/MeasureRectangle/Grids.java
@@ -7,12 +7,16 @@ import java.awt.Graphics;
 @SuppressWarnings("serial")
 class Grids extends Canvas {
 	  int width, height, rows, columns;
+	  boolean wInvisible, hInvisible;
 
 	  Grids(int w, int h, int r, int c) {
 	    setSize(width = w, height = h);
 	    rows = r;
 	    columns = c;
+	    this.wInvisible = false;
+	    this.hInvisible = false;
 	  }
+	  
 	    public void paint(Graphics g) {
 	    int k;
 	    
@@ -20,15 +24,22 @@ class Grids extends Canvas {
 	    height = getSize().height;
 
 	    int htOfRow = height / (rows);
-	    for (k = 0; k < rows; k++)
-	      g.drawLine(0, k * htOfRow , width, k * htOfRow );
-
+	    
+	    if(!hInvisible){
+	    	for (k = 0; k < rows; k++)
+	    		g.drawLine(0, k * htOfRow , width, k * htOfRow );
+	    }
+	    if(!wInvisible){
 	    int wdOfRow = width / (columns);
 	    for (k = 0; k < columns; k++)
 	      g.drawLine(k*wdOfRow , 0, k*wdOfRow , height);
+	    }
 	  }
+		public void setHInvisible(boolean h){this.hInvisible = h;}
+		public void setWInvisible(boolean w){this.wInvisible = w;}
 	}
 	
+
 	@SuppressWarnings("serial")
 	class DrawGrids extends Frame {
 	  DrawGrids(String title, int w, int h, int rows, int columns) {
